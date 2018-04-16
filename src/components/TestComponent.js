88dfa3915eb77dfbb8730ea846e1c99b18c4db56
DDChiang <ddeechng@gmail.com>
2018-04-15 18:17:58 -0700

    add styled components package

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Text = styled.div`
+  background: ${ props => props.val > 4 ? 'grey' : '#ddd' }};
+`;
 
 export default class TestComponent extends Component {
   render() {
     return (
       <div>
         TestComponent
+        <Text val={ 3 } >Some Text</Text>
+        <Text val={ 7 } >Background should be darker based on value passed directly into styled component</Text>
       </div>
     );
   }
--- src/containers/app.js
+++ src/containers/app.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import styled from 'styled-components';
 
 import TestComponent from '../components/TestComponent';
+
+const Title = styled.h1`
+  background: red;
+`;
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello App
+        <Title>Hello App</Title>
         <TestComponent />
       </div>
     );
